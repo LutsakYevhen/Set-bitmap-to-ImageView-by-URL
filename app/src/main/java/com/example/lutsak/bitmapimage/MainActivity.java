dd7ab63f1be816334afb126773055773bacf7dfc
Lutsak Yevhen <yev.lutsak@gmail.com>
2018-06-17 15:01:09 +0300

    Bug during rotation need help

--- app/src/main/java/com/example/lutsak/bitmapimage/MainActivity.java
+++ app/src/main/java/com/example/lutsak/bitmapimage/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.lutsak.bitmapimage;
 
+import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
@@ -13,8 +14,9 @@ import java.net.URL;
 public class MainActivity extends AppCompatActivity {
 
     public static final String TAG = MainActivity.class.getSimpleName();
-
-    public static final String IMAGE_URL = "https://www.google.com/images/srpr/logo11w.png";
+    public static final String IMAGE_URL = "https://www.alcopa-auction.fr/assets/img/brand-volkswagen.png";
+    private static final int HALF_SCREEN_WIDTH = Resources.getSystem().getDisplayMetrics().widthPixels / 2;
+    private static final int HALF_SCREEN_HEIGHT = Resources.getSystem().getDisplayMetrics().heightPixels / 2;
 
     private ImageView mImageView;
     private DownLoadImageTask mLoadImageTask;
@@ -30,16 +32,20 @@ public class MainActivity extends AppCompatActivity {
           Download the logo from online and set it as
           ImageView image programmatically.
          */
-        mLoadImageTask = new DownLoadImageTask(mImageView);
-        mLoadImageTask.execute(IMAGE_URL);
+        mLoadImageTask = (DownLoadImageTask) getLastCustomNonConfigurationInstance();
+        if (mLoadImageTask == null) {
+            Log.d(TAG, "create");
+            mLoadImageTask = new DownLoadImageTask();
+            mLoadImageTask.execute(IMAGE_URL);
+        }
+        mLoadImageTask.link(this);
 
         Log.d(TAG, "<< onCreate ");
     }
 
-    @Override
-    public void onStop() {
-        super.onStop();
-        mLoadImageTask.cancel(true);
+    public Object onRetainCustomNonConfigurationInstance() {
+       mLoadImageTask.unLink();
+        return mLoadImageTask;
     }
 
     /*
@@ -48,22 +54,46 @@ public class MainActivity extends AppCompatActivity {
       thread without having to manipulate threads and/or handlers.
      */
     private static class DownLoadImageTask extends AsyncTask<String, Void, Bitmap> {
-        private ImageView imageView;
+        private MainActivity mActivity;
 
-        DownLoadImageTask(ImageView imageView) {
-            this.imageView = imageView;
+
+        void link(MainActivity act) {
+            mActivity = act;
+        }
+
+        void unLink() {
+            mActivity = null;
+        }
+
+        private static Bitmap resize(Bitmap image, int maxWidth, int maxHeight) {
+            if (maxHeight > 0 && maxWidth > 0) {
+                float ratioBitmap = (float) image.getWidth() / (float) image.getHeight();
+                float ratioMax = (float) maxWidth / (float) maxHeight;
+
+                int finalWidth = maxWidth;
+                int finalHeight = maxHeight;
+                if (ratioMax > ratioBitmap) {
+                    finalWidth = (int) ((float)maxHeight * ratioBitmap);
+                } else {
+                    finalHeight = (int) ((float)maxWidth / ratioBitmap);
+                }
+                image = Bitmap.createScaledBitmap(image, finalWidth, finalHeight, true);
+                return image;
+            } else {
+                return image;
+            }
         }
 
         @Override
         protected Bitmap doInBackground(String... urls) {
             Bitmap logo = null;
-
             try {
                 /*
                   decodeStream(InputStream)
                   Decode an input stream into a bitmap.
                  */
                 logo = BitmapFactory.decodeStream(new URL(IMAGE_URL).openStream());
+                logo = resize(logo, HALF_SCREEN_WIDTH, HALF_SCREEN_HEIGHT);
             } catch (IOException ioe) {  // Catch the download exception
                 Log.e(TAG, ioe.toString());
             }
@@ -73,10 +103,10 @@ public class MainActivity extends AppCompatActivity {
         @Override
         protected void onPostExecute(Bitmap result) {
             try {
-                imageView.setImageBitmap(result);
+                mActivity.mImageView.setImageBitmap(result);
             }
             catch (Exception ex){  //handle null image with default one
-                imageView.setImageResource(R.drawable.ic_launcher_background);
+                mActivity.mImageView.setImageResource(R.drawable.ic_launcher_background);
             }
         }
     }
